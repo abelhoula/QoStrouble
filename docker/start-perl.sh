f81bcfea116e90822cbb5289f57b003db4cb94da
ahmed_b <a.belhoula@openbee.com>
2019-05-25 18:56:07 +0200

    add & start-perl.sh

--- docker/start-perl.sh
+++ docker/start-perl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup perl ../perl/serv.pl
-nohup perl ../perl/excute.pl
-nohup perl ../perl/debit2.pl
-nohup perl ../perl/kill2.pl
+nohup perl ../perl/serv.pl &
+nohup perl ../perl/excute.pl &
+nohup perl ../perl/debit2.pl &
+nohup perl ../perl/kill2.pl &
